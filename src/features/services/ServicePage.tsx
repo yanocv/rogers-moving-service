ebb075c78c5a9065461e8a4e8eca19bde7fb6a06
Yano <51319482+yanocv@users.noreply.github.com>
2025-10-07 23:32:02 +0900

    fix: styles

--- src/features/services/ServicePage.tsx
+++ src/features/services/ServicePage.tsx
@@ -2,19 +2,24 @@
 
 import { useTranslations } from "next-intl";
 
-import { ServiceList } from "./ServiceList";
+import FooterSection from "@/features/landing-page/components/FooterSection";
+
+import { ServiceList } from "./components/ServiceList";
 
 export default function ServicePage() {
   const t = useTranslations("services");
   return (
-    <section id="services" className="bg-gray-50 py-20">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="mb-16 text-center">
-          <h2 className="mb-4 text-4xl font-bold text-gray-900 md:text-5xl">{t("title")}</h2>
-          <p className="mx-auto max-w-3xl text-xl text-gray-600">{t("desc")}</p>
+    <>
+      <section id="services" className="bg-gray-50 py-24">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mb-16 text-center">
+            <h2 className="mb-4 text-4xl font-bold text-gray-900 md:text-5xl">{t("title")}</h2>
+            <p className="mx-auto max-w-3xl text-xl text-gray-600">{t("desc")}</p>
+          </div>
+          <ServiceList />
         </div>
-        <ServiceList />
-      </div>
-    </section>
+      </section>
+      <FooterSection />
+    </>
   );
 }
